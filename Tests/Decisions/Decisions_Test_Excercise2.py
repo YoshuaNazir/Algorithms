7a8be479bb9608f11a9a8b120c1228805bb435f7
YoshuaNazir <yoshuanazir@gmail.com>
2021-10-11 09:05:14 -0400

    Incomplete Decisions Exercise 2 and Test

---
+++ Tests/Decisions/Decisions_Test_Excercise2.py
@@ -0,0 +1,31 @@
+import unittest
+
+
+def count_digit_number(n):
+    count: int = 0
+    while n != 0:
+        n //= 10
+        count += 1
+    return count
+
+def number_is_negative(n):
+    if n < 0:
+        return 1
+    else:
+        0
+
+
+class MyTestCase(unittest.TestCase):
+    def number_is_negative(self):
+        self.assertTrue(-1)  # If number is negative needs to be below 0.
+        self.assertEqual(12, False)
+        self.assertRaises(ValueError, number_is_negative(5))
+
+
+    def count_digit_number(self):
+        self.assertEqual()
+
+
+if __name__ == '__main__':
+    unittest.main()
+#Notes: The functions above do not the desired tasks yet and need to be revised, this excercise is still incomplete.
