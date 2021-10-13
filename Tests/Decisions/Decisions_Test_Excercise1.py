20f28a8bf68a7a0be0d020025790e588732a0b31
YoshuaNazir <yoshuanazir@gmail.com>
2021-10-12 22:50:54 -0400

    Fixing Test escercise 1

--- Tests/Decisions/Decisions_Test_Excercise1.py
+++ Tests/Decisions/Decisions_Test_Excercise1.py
@@ -1,16 +1,14 @@
-#1. Leer un número entero y determinar si es un número terminado en 4.
-
 import unittest
 
-def second_digit_value4(n): return n % 10
+
+def second_digit_value4(n): return 1 if n % 10 == 4 else 0
 
 
-class Wholenumberseconddigit4(unittest.TestCase):
-    def test_seconddigitis4(self):
-        self.assertEqual(second_digit_value4(14), 4)  # Is a whole number
-        self.assertEqual(second_digit_value4(24), 4)
-        self.assertNotEqual(25, 5)  # Should not be equal to 4
-        self.assertNotEqual(48, 8)
+class DigitValue4(unittest.TestCase):
+    def test_last_digit_value4(self):
+        self.assertEqual(second_digit_value4(14), True)
+        self.assertFalse(second_digit_value4(22), False)
+
 
 if __name__ == '__main__':
     unittest.main()
