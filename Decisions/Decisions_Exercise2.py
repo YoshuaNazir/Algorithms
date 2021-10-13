91b0b29c79aeda047fee62db82baeec886ca176f
YoshuaNazir <yoshuanazir@gmail.com>
2021-10-12 21:05:12 -0400

    Pushing Decisions excercise 2 and test

---
+++ Decisions/Decisions_Exercise2.py
@@ -0,0 +1,5 @@
+# 2. Leer un número entero y determinar si tiene 3 dígitos.
+
+def wholethreedigits(n):
+    if n >= 100 and n <= 999:
+        print("This number has 3 digits")
--- Tests/Decisions/Decisions_Test_Excercise2.py
+++ Tests/Decisions/Decisions_Test_Excercise2.py
@@ -1,31 +1,14 @@
 import unittest
 
-
-def count_digit_number(n):
-    count: int = 0
-    while n != 0:
-        n //= 10
-        count += 1
-    return count
-
-def number_is_negative(n):
-    if n < 0:
-        return 1
-    else:
-        0
-
+def wholethreedigits(n):
+    if n >= 100 and n <= 999: return True
 
 class MyTestCase(unittest.TestCase):
-    def number_is_negative(self):
-        self.assertTrue(-1)  # If number is negative needs to be below 0.
-        self.assertEqual(12, False)
-        self.assertRaises(ValueError, number_is_negative(5))
+    def test_wholethreedigit(self):
+        self.assertTrue(wholethreedigits(666))
+        self.assertFalse(wholethreedigits(66))
 
-
-    def count_digit_number(self):
-        self.assertEqual()
 
 
 if __name__ == '__main__':
     unittest.main()
-#Notes: The functions above do not complete the desired tasks yet and need to be revised, this excercise is still incomplete.
