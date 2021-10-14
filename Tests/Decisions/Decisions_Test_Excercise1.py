77985baa23d82d1d0c3275920d8728ca99794642
YoshuaNazir <yoshuanazir@gmail.com>
2021-10-14 08:55:48 -0400

    Adding value error to excercise

--- Tests/Decisions/Decisions_Test_Excercise1.py
+++ Tests/Decisions/Decisions_Test_Excercise1.py
@@ -1,13 +1,33 @@
 import unittest
 
+def iswholenumber(n):
+    if type(n) == int:
+        return True
+    elif type(n) != int:
+        raise ValueError("Is not a whole number")
 
-def second_digit_value4(n): return 1 if n % 10 == 4 else 0
-
+def last_digit_value4(n):
+    if n % 10 == 4:
+        return True
+    elif n % 10 != 4:
+        raise ValueError("The last digit is not equals 4")
 
 class DigitValue4(unittest.TestCase):
     def test_last_digit_value4(self):
-        self.assertEqual(second_digit_value4(14), True)
-        self.assertFalse(second_digit_value4(22), False)
+        self.assertEqual(last_digit_value4(14), True)
+        self.assertRaises(ValueError, last_digit_value4, 12)
+        self.assertTrue(last_digit_value4(24))
+
+        with self.assertRaises(ValueError):
+            last_digit_value4(23)
+
+    def test_iswholenumber(self):
+        self.assertEqual(iswholenumber(123), True)
+        self.assertTrue(iswholenumber(5))
+
+        with self.assertRaises(ValueError):
+            iswholenumber(3.1)
+
 
 
 if __name__ == '__main__':
