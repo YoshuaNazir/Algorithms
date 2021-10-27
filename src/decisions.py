0f126e0534d8ad510acee99ea544e64c0286ed78
YoshuaNazir <yoshuanazir@gmail.com>
2021-10-26 20:11:16 -0400

    Decisions excercise 5 with test

--- src/decisions.py
+++ src/decisions.py
@@ -15,3 +15,15 @@ class Decisions:
         """Determines if the number has three digits """
         if n < 100 or n > 999:
             raise ValueError("Please introduce a 3 digit number")
+
+    def is_negative(self, n):
+        """Determines if the number is negative"""
+        return n < 0
+
+    def sum_two_digits(self, n):
+        if n >= 10 and n <= 99:
+            return n // 10 + n % 10
+
+    def two_digit_pair(self, n):
+        if n >= 10 and n <= 99:
+            return (n // 10) % 2 == 0 and (n % 10) % 2 == 0
--- test/Decisions_Test_Excercise1.py
+++ test/Decisions_Test_Excercise1.py
@@ -30,5 +30,31 @@ class Value4(unittest.TestCase):
         self.assertRaises(ValueError, self.decisions.whole_three_digits, 6)
         self.assertRaises(ValueError, self.decisions.whole_three_digits, 77)
 
+    def test_is_negative(self):
+        self.assertTrue(self.decisions.is_negative(-5))
+        self.assertNotEqual(self.decisions.is_negative(-66), False)
+        self.assertEqual(self.decisions.is_negative(-888), True)
+        self.assertTrue(self.decisions.is_negative(-0.5))
+        self.assertNotEqual(self.decisions.is_negative(33), True)
+        self.assertEqual(self.decisions.is_negative(999), False)
+
+    def test_sum_two_digits(self):
+        self.assertEqual(self.decisions.sum_two_digits(99), 18)
+        self.assertEqual(self.decisions.sum_two_digits(69), 15)
+        self.assertNotEqual(self.decisions.sum_two_digits(43), 9)
+        self.assertNotEqual(self.decisions.sum_two_digits(44), 5)
+        self.assertEqual(self.decisions.sum_two_digits(11), 2)
+        self.assertNotEqual(self.decisions.sum_two_digits(22), 15)
+
+    def test_two_digit_pair(self):
+        self.assertEqual(self.decisions.two_digit_pair(44), True)
+        self.assertEqual(self.decisions.two_digit_pair(14), False)
+        self.assertNotEqual(self.decisions.two_digit_pair(19), True)
+        self.assertNotEqual(self.decisions.two_digit_pair(11), True)
+        self.assertEqual(self.decisions.two_digit_pair(22), True)
+        self.assertNotEqual(self.decisions.two_digit_pair(88), False)
+        self.assertTrue(self.decisions.two_digit_pair(24))
+
+
 if __name__ == '__main__':
     unittest.main()
