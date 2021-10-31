2774fa55e55a213b6b4c307f43f6156e0c5e83dc
YoshuaNazir <yoshuanazir@gmail.com>
2021-10-30 22:57:28 -0400

    Submitting loops

---
+++ src/loops.py
@@ -0,0 +1,5 @@
+class Loops:
+
+    def show_numbers(self, n):
+        for i in range(1, n):
+            print(i)
---
+++ test/Loops_Tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+from src.loops import Loops
+
+class MyTestCase(unittest.TestCase):
+
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
