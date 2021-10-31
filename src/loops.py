7d4e2171f62d8b088e723564d5e892e865d9d6ad
YoshuaNazir <yoshuanazir@gmail.com>
2021-10-30 23:19:04 -0400

    Submitting excercise 2

--- src/loops.py
+++ src/loops.py
@@ -1,5 +1,11 @@
 class Loops:
-
+"""Showing numbers from 1 to the positive number"""
     def show_numbers(self, n):
         for i in range(1, n):
             print(i)
+
+"""Showing pairs from 1 to the introduced number"""
+    def show_pairs(n):
+        for i in range(1, n):
+            if i % 2 == 0:
+                print (i)
