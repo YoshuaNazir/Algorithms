159208fdfd7d7b9b033753b6896f4f0d6a409578
YoshuaNazir <yoshuanazir@gmail.com>
2021-10-31 22:36:58 -0400

    Pushing loops excercise 4

--- src/loops.py
+++ src/loops.py
@@ -1,11 +1,30 @@
 class Loops:
-"""Showing numbers from 1 to the positive number"""
+
+    # Showing numbers from 1 to the positive number
     def show_numbers(self, n):
         for i in range(1, n):
             print(i)
 
-"""Showing pairs from 1 to the introduced number"""
+    # Showing pairs from 1 to the introduced number
     def show_pairs(self, n):
         for i in range(1, n):
             if i % 2 == 0:
-                print (i)
+                print(i)
+
+    # Showing exact dividers of the introduced numbers
+    def show_dividers(n):
+        for i in range(1, n):
+            if n % i == 0:
+                print(i)
+
+
+    def show_whole_numbers(self,n1,n2):
+        if n1 < n2:
+            First = n1
+            Second = n2
+        if n2 < n1:
+            First = n2
+            Second = n1
+        for i in range(First, Second):
+            if i > 0:
+            print(i)
--- test/Loops_Tests.py
+++ test/Loops_Tests.py
@@ -4,8 +4,20 @@ from src.loops import Loops
 
 class MyTestCase(unittest.TestCase):
 
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def setup(self):
+        self.loops = Loops()
+
+    def show_numbers(self):
+        for i in range(1, 10):
+            with self.subTest(self):
+                self.assertEqual(i, i)
+
+    def show_pairs(self):
+        for i in range(1, 10):
+            with self.show_pairs(self):
+                self.assertTrue(i % 2, 0)
+
+
 
 
 if __name__ == '__main__':
